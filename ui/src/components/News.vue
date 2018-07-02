1609d6f2a4470e91e38cec048d695741fa0422cb
BeanWei <157318439@qq.com>
2018-07-02 16:11:51 +0800

    修改前端样式

--- ui/src/components/News.vue
+++ ui/src/components/News.vue
@@ -2,37 +2,31 @@
   <v-layout row style="margin-top: -10px;">
     <v-flex>
       <v-card>
-        <!-- <v-toolbar color="grey darken-4" dark tabs> -->
-          <!-- <v-toolbar-title>新闻头条</v-toolbar-title>
-          <v-spacer></v-spacer>
-          <v-btn icon>
-            <v-icon>search</v-icon>
-          </v-btn> -->
-        <v-tabs dark show-arrows fixed-tabs>
+        <v-tabs dark show-arrows fixed-tabs v-model="currentItem">
           <v-tabs-slider color="red"></v-tabs-slider>
-          <v-tab v-for="(item, index) in items" :href="'#tab-' + index" :key="index" color="black">{{item}}</v-tab>
+          <v-tab v-for="item in mainitems" :href="'#tab-' + item" color="black">{{item}}</v-tab>
           <v-menu
             bottom
             class="v-tabs__div"
             left
+            auto
+            nudge-bottom="50"
           >
             <a slot="activator" class="v-tabs__item">
               更多
               <v-icon>add</v-icon>
             </a>
-
             <v-list class="grey lighten-3">
               <v-list-tile
-                v-for="item in items"
-                :key="item"
-                @click="addItem(item)"
+                v-for="item in allitems"
+                :href="'#tab-' + item"
+                @click="more(item)"
               >
                 {{ item }}
               </v-list-tile>
             </v-list>
           </v-menu>
         </v-tabs>
-        <!-- </v-toolbar> -->
         <v-container fluid grid-list-md>
           <v-layout row wrap>
             <v-dialog v-model="browse" width="600px">
@@ -45,12 +39,10 @@
               </v-card>
             </v-dialog>
             <v-flex xs12 sm7 style="margin-top: -32px;">
-              <!-- <v-tabs-items >
-                <v-tab-item v-for="(item, index) in items" :id="'tab-' + index" :key="index"> -->
               <v-list three-line>
                 <template v-for="(news, index) in newslist">
                   <v-divider :divider="false" :inset="true" :key="index"></v-divider>
-                  <v-list-tile :key="news.url" avatar @click="seeout(news.url)">
+                  <v-list-tile avatar @click="seeout(news.url)">
                     <v-list-tile-avatar :size="72" :tile="true">
                       <img :src="news.cover">
                     </v-list-tile-avatar>
@@ -69,8 +61,6 @@
                   </v-list-tile>
                 </template>
               </v-list>
-                <!-- </v-tab-item>
-              </v-tabs-items> -->
             </v-flex>
             <v-flex xs12 sm4 offset-xs1 style="margin-top: -32px;">
               <v-text-field
@@ -83,7 +73,7 @@
                 <v-subheader><v-icon color="red">whatshot</v-icon><strong>24小时热榜</strong></v-subheader>
                 <template v-for="(news, index) in newslist">                
                   <!-- <v-divider :divider="false" :inset="false" :key="index"></v-divider> -->
-                  <v-list-tile :key="news.url" avatar @click="seeout(news.url)">
+                  <v-list-tile avatar @click="seeout(news.url)">
                     <v-list-tile-content>
                       <v-list-tile-title>{{news.title}}</v-list-tile-title>
                       <v-list-tile-sub-title>
@@ -110,24 +100,29 @@ export default {
     return {
       resp: '',
       browse: false,
-      items:[
-       "科技",
-       "娱乐",
-       "搞笑",
-       "财经",
-       "游戏",
-      ],
+      currentItem: 'tab-0',
+      mainitems: ["热点","科技","娱乐","搞笑","财经","游戏"],
+      allitems: ["热点","科技","娱乐","搞笑","财经","游戏", "足球","历史","电影","时政","健康","教育"],
       newslist: [
-        {url: "http://news.ynet.com/2018/07/02/1287606t70.html", cover:"https://www.baidu.com/img/bd_logo1.png", title: "詹姆斯与湖人队签订4年1.54亿美元合同", author:"北京青年报-北青网", read:"520", comment:"66", publishtime:"2018-07-02 08:37:45"},
-        {url: "http://news.ynet.com/2018/07/02/1287606t70.html", cover:"https://www.baidu.com/img/bd_logo1.png", title: "詹姆斯与湖人队签订4年1.54亿美元合同", author:"北京青年报-北青网", read:"520", comment:"66", publishtime:"2018-07-02 08:37:45"},
-        {url: "http://news.ynet.com/2018/07/02/1287606t70.html", cover:"https://www.baidu.com/img/bd_logo1.png", title: "詹姆斯与湖人队签订4年1.54亿美元合同", author:"北京青年报-北青网", read:"520", comment:"66", publishtime:"2018-07-02 08:37:45"},
-        {url: "http://news.ynet.com/2018/07/02/1287606t70.html", cover:"https://www.baidu.com/img/bd_logo1.png", title: "詹姆斯与湖人队签订4年1.54亿美元合同", author:"北京青年报-北青网", read:"520", comment:"66", publishtime:"2018-07-02 08:37:45"},
-        {url: "http://news.ynet.com/2018/07/02/1287606t70.html", cover:"https://www.baidu.com/img/bd_logo1.png", title: "詹姆斯与湖人队签订4年1.54亿美元合同", author:"北京青年报-北青网", read:"520", comment:"66", publishtime:"2018-07-02 08:37:45"},
-        {url: "http://news.ynet.com/2018/07/02/1287606t70.html", cover:"https://www.baidu.com/img/bd_logo1.png", title: "詹姆斯与湖人队签订4年1.54亿美元合同", author:"北京青年报-北青网", read:"520", comment:"66", publishtime:"2018-07-02 08:37:45"}
+        {url: "", cover:"https://www.baidu.com/img/bd_logo1.png", title: "詹姆斯与湖人队签订4年1.54亿美元合同", author:"北京青年报-北青网", read:"520", comment:"66", publishtime:"2018-07-02 08:37:45"},
+        {url: "", cover:"https://www.baidu.com/img/bd_logo1.png", title: "詹姆斯与湖人队签订4年1.54亿美元合同", author:"北京青年报-北青网", read:"520", comment:"66", publishtime:"2018-07-02 08:37:45"},
+        {url: "", cover:"https://www.baidu.com/img/bd_logo1.png", title: "詹姆斯与湖人队签订4年1.54亿美元合同", author:"北京青年报-北青网", read:"520", comment:"66", publishtime:"2018-07-02 08:37:45"},
+        {url: "", cover:"https://www.baidu.com/img/bd_logo1.png", title: "詹姆斯与湖人队签订4年1.54亿美元合同", author:"北京青年报-北青网", read:"520", comment:"66", publishtime:"2018-07-02 08:37:45"},
+        {url: "", cover:"https://www.baidu.com/img/bd_logo1.png", title: "詹姆斯与湖人队签订4年1.54亿美元合同", author:"北京青年报-北青网", read:"520", comment:"66", publishtime:"2018-07-02 08:37:45"},
+        {url: "", cover:"https://www.baidu.com/img/bd_logo1.png", title: "詹姆斯与湖人队签订4年1.54亿美元合同", author:"北京青年报-北青网", read:"520", comment:"66", publishtime:"2018-07-02 08:37:45"}
       ]
     }
   },
+  watch: {
+    "currentItem": function(val, oldval) {
+      const nowitem = val.slice(4)
+      console.log(nowitem)
+    }
+  },
   methods: {
+    more(item) {
+      this.currentItem = 'tab-' + item
+    },
     seeout(url) {
       this.$axios.get(
         url,
