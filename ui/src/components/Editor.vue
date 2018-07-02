228b3fa3afd89a7f6c8b42a68a179561d800de59
BeanWei <157318439@qq.com>
2018-07-02 16:54:42 +0800

    修改文本编辑器样式

--- ui/src/components/Editor.vue
+++ ui/src/components/Editor.vue
@@ -1,57 +1,74 @@
 <template>
-  <v-card>
-    <div id="toolbar"></div>
-    <div id="content">
-      <p>请输入内容</p>
-    </div>
-    <!-- <div id="editorElem" style="text-align:left">
-      <p>欢迎使用富文本编辑器</p>
-    </div> -->
-    <v-speed-dial
-      v-model="fab"
-      bottom
-      right
-      fixed
-      :direction="direction"
-      :open-on-hover="hover"
-      :transition="transition"
-    >
-      <v-btn
-        slot="activator"
-        v-model="fab"
-        color="teal"
-        dark
-        fab
-      >
-        <v-icon>widgets</v-icon>
-        <v-icon>close</v-icon>
-      </v-btn>
-      <v-btn
-        fab
-        dark
-        small
-        color="green"
-      >
-        <v-icon>edit</v-icon>
-      </v-btn>
-      <v-btn
-        fab
-        dark
-        small
-        color="indigo"
-      >
-        <v-icon>add</v-icon>
-      </v-btn>
-      <v-btn
-        fab
-        dark
-        small
-        color="red"
-      >
-        <v-icon>delete</v-icon>
-      </v-btn>
-    </v-speed-dial>
-  </v-card>
+  <v-container fluid>
+    <v-layout row>
+      <v-flex xs12 sm12 style="margin-top: -20px;">
+        <v-text-field
+          v-model="title"
+          :rules="[titlerules.required, titlerules.counter]"
+          label="文章标题"
+          counter
+          maxlength="50"
+        ></v-text-field>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs12 sm12>
+        <v-card style="margin-top: 12px;">
+          <div id="toolbar"></div>
+          <div id="content">
+            <p>请输入内容</p>
+          </div>
+          <!-- <div id="editorElem" style="text-align:left">
+            <p>欢迎使用富文本编辑器</p>
+          </div> -->
+          <v-speed-dial
+            v-model="fab"
+            bottom
+            right
+            fixed
+            :direction="direction"
+            :open-on-hover="hover"
+            :transition="transition"
+          >
+            <v-btn
+              slot="activator"
+              v-model="fab"
+              color="teal"
+              dark
+              fab
+            >
+              <v-icon>widgets</v-icon>
+              <v-icon>close</v-icon>
+            </v-btn>
+            <v-btn
+              fab
+              dark
+              small
+              color="green"
+            >
+              <v-icon>edit</v-icon>
+            </v-btn>
+            <v-btn
+              fab
+              dark
+              small
+              color="indigo"
+            >
+              <v-icon>add</v-icon>
+            </v-btn>
+            <v-btn
+              fab
+              dark
+              small
+              color="red"
+            >
+              <v-icon>delete</v-icon>
+            </v-btn>
+          </v-speed-dial>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -60,7 +77,12 @@ export default {
   data() {
     return{
       fab: false,
+      title: '',
       editorContent: '',
+      titlerules: {
+        required: value => !!value || '标题不能为空.',
+        counter: value => value.length <= 50 || '标题限制50字',
+      }
     }
   },
   mounted() {
