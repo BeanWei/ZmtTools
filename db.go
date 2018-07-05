6dd0b3fa51e8fb0d432ef464557b52358edfcd00
BeanWei <157318439@qq.com>
2018-07-05 15:11:41 +0800

    重写db逻辑

--- db.go
+++ db.go
@@ -7,13 +7,23 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func dbinit() *sql.DB {
-	//打开数据库，如果不存在，则创建
-	db, err := sql.Open("sqlite3", "./src.db")
-	if err != nil {
-		log.Fatal("Line14-Error: ", err)
-	}
+type Storage struct {
+	Filename	string
+	dbh	*sql.DB
+}
 
+func (s *Storage) Init() error {
+	if s.dbh == nil {
+		db, err := sql.Open("sqlite3", s.Filename)
+		if err != nil {
+			return err
+		}
+		err = db.Ping()
+		if err != nil {
+			return err
+		}
+		s.dbh = db
+	}
 	//创建表
 	sql_table := `
 	 CREATE TABLE IF NOT EXISTS newsinfo(
@@ -28,22 +38,39 @@ func dbinit() *sql.DB {
 			 cover	TEXT	NULL
 	 );
 	 `
-
-	db.Exec(sql_table)
-
-	return db
-}
-
-func newsadd(db *sql.DB, field string, title string, author string, publishtime string, views int, comments int, url string, cover string) error {
-
-	//插入数据
-	stmt, err := db.Prepare(
-		"INSERT INTO newsinfo(field, title, author, publishtime, views, comments, url, cover) values(?, ?, ?, ?, ?, ?, ?, ?)")
+	statement, _ := s.dbh.Prepare(sql_table)
+	_, err := statement.Exec()
 	if err != nil {
-		log.Fatal("Line43-Error: ", err)
 		return err
 	}
-	_, err = stmt.Exec(
+	return nil
+}
+
+func (s *Storage) Clear() error {
+	statement, err := s.dbh.Prepare("DROP TABLE newsinfo")
+	if err != nil {
+		return err
+	}
+	_, err = statement.Exec()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *Storage) Close() error {
+	err := s.dbh.Close()
+	return err
+}
+
+func (s *Storage) AddNews(field string, title string, author string, publishtime string, views int, comments int, url string, cover string) error {
+	//插入数据
+	statement, err := s.dbh.Prepare(
+		"INSERT INTO newsinfo(field, title, author, publishtime, views, comments, url, cover) values(?, ?, ?, ?, ?, ?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	_, err = statement.Exec(
 		field,
 		title,
 		author,
@@ -53,12 +80,9 @@ func newsadd(db *sql.DB, field string, title string, author string, publishtime 
 		url,
 		cover)
 	if err != nil {
-		log.Fatal("Line56-Error: ", err)
 		return err
 	}
-
 	return nil
-
 }
 
 // Newsinfo 结构体
@@ -73,77 +97,61 @@ type Newsinfo struct {
 	cover       string
 }
 
-// newsquery 根据选择的领域进行数据库查询筛选
-func newsquery(field string) []*Newsinfo {
-	db := dbinit()
-	rows, err := db.Query("SELECT * FROM newsinfo WHERE field=?", field)
+// Newsquery 根据选择的领域进行数据库查询筛选
+func (s *Storage) Newsquery(field string) ([]*Newsinfo, error) {
+	rows, err := s.dbh.Query("SELECT * FROM newsinfo WHERE field=?", field)
 	if err != nil {
-		log.Fatal("Line81-Error: ", err)
-		return nil
+		return nil, err
 	}
 	defer rows.Close()
-
 	allnews := []*Newsinfo{}
 	for rows.Next() {
 		n := new(Newsinfo)
 		err := rows.Scan(&n.field, &n.title, &n.author, &n.publishtime, &n.views, &n.comments, &n.url, &n.cover)
 		if err != nil {
-			log.Fatal("Line91-Error: ", err)
-			return nil
+			return nil, err
 		}
 		allnews = append(allnews, n)
 	}
-	return allnews
-}
+	return allnews, nil
+} 
 
-// domains 查询数据库中的所有领域
-func domains() []string {
-	db := dbinit()
-	rows, err := db.Query("SELECT field FROM newsinfo")
+// Domains 查询数据库中的所有领域
+func (s *Storage) Domains() ([]string, error) {
+	rows, err := s.dbh.Query("SELECT field FROM newsinfo")
 	if err != nil {
-		log.Fatal("Line104-Error: ", err)
-		return nil
+		return nil, err
 	}
 	results := []string{}
 	for rows.Next() {
 		var field string
 		err = rows.Scan(&field)
 		if err != nil {
-			log.Fatal("Line112-Error: ", err)
-			return nil
+			return nil,err
 		}
 		results = append(results, field)
 	}
-	return results
+	//数组去重
+	return RemRepArry(results), nil
 }
 
-//判断是否已存在该新闻
-func exist(db *sql.DB, url string) bool {
-	rows, err := db.Query("SELECT * FROM newsinfo WHERE url=?", url)
+// ExistURL 判断是否已存在该新闻
+func (s *Storage) ExistURL(url string) bool {
+	var count int
+	statement, err := s.dbh.Prepare("SELECT COUNT(*) FROM newsinfo where url = ?")
 	if err != nil {
-		log.Fatal("Line103-Error: ", err)
-		return false
+		 log.Printf("Query ExistURL failure: %s", err.Error())
+		 return true
 	}
-	if rows != nil {
+	row := statement.QueryRow(url)
+	err = row.Scan(&count)
+	if err != nil {
+		log.Fatal(err)
 		return true
-	} else {
-		return false
+ 	}
+	if count >= 1 {
+		return true
 	}
+	return false
 }
 
-// newsdelete 清空数据库表 TODO: 暂支持手动清空
-func newsdelete() error {
-	db := dbinit()
-	stmt, err := db.Prepare("DELETE FROM newsinfo")
-	if err != nil {
-		log.Fatal("Line104-Error: ", err)
-		return err
-	}
-	_, err = stmt.Exec()
-	if err != nil {
-		log.Fatal("Line110-Error: ", err)
-		return err
-	}
-
-	return nil
-}
